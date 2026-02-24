ff3c467913003ed52f8044d4aac74580fb06075f
yagneshraval <yagneshraval1407@gmail.com>
2026-02-24 11:07:22 +0530

    Add basic arithmetic operations in P1.c

---
+++ P1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d", &a, &b);
+
+    printf("The Sum of two number is %d\n", a+b);
+    printf("The Subtraction of two number is %d\n", a-b);
+    printf("The Multiplication of two number is %d\n", a*b);
+    printf("The Division of two number is %d\n", a/b);
+    printf("The Remainder is %d\n", a%b);
+    return 0;
+}
